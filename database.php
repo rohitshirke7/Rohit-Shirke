367298b745344562e8b02f753008c56bdcd96bbe
rohitshirke7 <47102004+rohitshirke7@users.noreply.github.com>
2019-01-28 14:37:18 +0530

    Update database.php

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ class Database{
  
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "PHPLearning";
+    private $db_name = "Rohitakshi";
     private $username = "root";
     private $password = "";
     public $conn;
